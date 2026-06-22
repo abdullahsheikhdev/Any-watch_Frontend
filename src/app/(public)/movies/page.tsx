e15458d83f743766d8bc159a4378bbb36d5e826a
Md Abdullah Sheikh <mbabdullahsk4220@gmail.com>
2026-06-22 14:29:58 +0600

    ok

---
+++ src/app/(public)/movies/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function MoviesPage() {
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      <p>This is the movies page.</p>
+    </div>
+  );
+}
---
+++ src/app/(public)/theaters/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function TheatersPage() {
+  return (
+    <div>
+      <h1>Theaters Page</h1>
+      <p>This is the theaters page.</p>
+    </div>
+  );
+}
